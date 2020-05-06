15f38bcb38fe81399069cb7111d71ac6332ae48a
Joseph Hung <arixxle@gmail.com>
2020-05-06 17:35:10 +0800

    create artile temp level

--- app/controllers/typing_controller.rb
+++ app/controllers/typing_controller.rb
@@ -1,10 +1,23 @@
 class TypingController < ApplicationController
   def index
-    
+    @articles = Article.all
   end
   def show
   end
+  def new
+    @articles = Article.new
+  end
   def create
-    render html: 'Create something'
+    @articles = Article.new(clean_article)
+    if @articles.save
+      redirect_to root_path
+    else
+      render json: params
+    end
+  end
+
+  private
+  def clean_article
+    params.require(:article).permit(:content)
   end
 end
