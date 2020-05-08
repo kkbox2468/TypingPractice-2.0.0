1f477b54ac098e89fc398ac7fc1c1bd806a23348
Joseph Hung <arixxle@gmail.com>
2020-05-08 16:23:17 +0800

    add model Code

--- db/schema.rb
+++ db/schema.rb
@@ -35,12 +35,6 @@ ActiveRecord::Schema.define(version: 2020_05_08_054823) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "codes", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
