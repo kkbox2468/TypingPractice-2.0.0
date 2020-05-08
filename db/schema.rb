0891379a7a5c8d61f118251fcbeb1f28ddcfd221
Will <willie91734563@gmail.com>
2020-05-08 14:21:04 +0800

    fixed 新增admin功能(remove_index(:admins, :reset_password_token))

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_160655) do
+ActiveRecord::Schema.define(version: 2020_05_08_054823) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2020_05_06_160655) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"reset_password_token\"", name: "index_admins_on_reset_password_token", unique: true
     t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
