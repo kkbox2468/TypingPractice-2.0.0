c76fd14b2aeac60cdc6165148bcd408f11271099
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-08 10:50:53 +0800

    add user_setting add stats

--- test/controllers/typing_controller_test.rb
+++ test/controllers/typing_controller_test.rb
@@ -4,7 +4,4 @@ class TypingControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  prompt_context() {
-    prompt_segment $CURRENT_BG default  "▙ ▞◼▖▜ ▚"
-  }
 end
