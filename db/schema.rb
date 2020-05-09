6617c06bed3b69733c687999a57518396be93562
Joseph Hung <arixxle@gmail.com>
2020-05-09 09:56:11 +0800

    fix schema conflicted

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_05_08_054823) do
+ActiveRecord::Schema.define(version: 2020_05_08_082239) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +29,12 @@ ActiveRecord::Schema.define(version: 2020_05_08_054823) do
   end
 
   create_table "articles", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "codes", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
