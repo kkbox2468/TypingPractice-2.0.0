a1e300d8cf960225ac45c34e6e69caf368ed6538
Will <willie91734563@gmail.com>
2020-05-11 14:49:29 +0800

    users table articles table many to many

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   validates :content, presence: true
+
+
+  has_many :user_articles
+  has_many :users, through: :user_articles
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+  has_many :user_articles
+  has_many :articles, through: :user_articles
 end
