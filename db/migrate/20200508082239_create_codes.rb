536257fb032256b639e617a69b88666cf61f2d2e
Joseph Hung <arixxle@gmail.com>
2020-05-12 10:44:08 +0800

    remove code model and db

--- db/migrate/20200508082239_create_codes.rb
+++
@@ -1,9 +0,0 @@
-class CreateCodes < ActiveRecord::Migration[6.0]
-  def change
-    create_table :codes do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 ActiveRecord::Schema.define(version: 2020_05_11_062858) do
 
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_062858) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 
   create_table "rubies", force: :cascade do |t|
 
@@ -60,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_062858) do
     t.index ["article_id"], name: "index_user_articles_on_article_id"
     t.index ["user_id"], name: "index_user_articles_on_user_id"
   end
+
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
