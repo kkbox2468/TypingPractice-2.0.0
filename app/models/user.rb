ff7bd85f05c7b0908bae89cf98cf5d9bf29ec274
Will <willie91734563@gmail.com>
2020-05-13 01:04:56 +0800

    validates_uniqueness_of_nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  validates_uniqueness_of :nickname
 
 
   has_many :user_articles
