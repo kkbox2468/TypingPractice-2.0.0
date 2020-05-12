5e32697a09d0cea57cee522fb1625639b964f54c
inorgpod <59144564+inorgpod@users.noreply.github.com>
2020-05-12 15:21:37 +0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
          :recoverable, :rememberable, :validatable
 
 
 
   has_many :user_articles
   has_many :articles, through: :user_articles
-=======
-          :recoverable, :rememberable, :validatable
->>>>>>> welcome_canvas
 end
