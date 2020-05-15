805348f06215e926dec017c0b2250fa317edfaa9
EnTzu <tainn_50554@yahoo.com.tw>
2020-05-15 18:29:09 +0800

    fix conflicted

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,17 @@
 class StatsController < ApplicationController
   def index
+    find_type_history  
+    
+  end
+
+  private
+
+  def find_type_history 
+    @user_article = UserArticle.find_by(params[:id])
+  end
+
+  
+  def article_params
+    params.require(:user_article).permit(:time, :speed, :user_id, :article_id)
   end
 end
