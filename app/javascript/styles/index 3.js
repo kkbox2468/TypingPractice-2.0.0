554a7dfc9cbfbc387164885d8ef861f5471bfc1d
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-18 12:11:51 +0800

    rebase master

---
+++ app/javascript/styles/index 3.js
@@ -0,0 +1,16 @@
+//公共區
+import './_navbar.css'
+import './application.css'
+
+//
+import './welcome.css'
+
+import './playground.css'
+
+import './mode.css'
+
+import './typing.css'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+// import '@fortawesome/fontawesome-free/css/all.css'
