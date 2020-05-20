11ada56bfa558c2408cb76d1557eb2b576b7bb1a
Gao Ciaorun <itchynozz@gmail.com>
2020-05-20 11:42:16 +0800

    chart.js installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,11 @@ require("@rails/activestorage").start()
 require("styles")
 require("scripts")
 
-//= require Chart
+//chart.js
+import Chart from 'chart.js';
+Windows.Chart = Chart;
+// var chart = new Chart(ctx, { }); //--->官網寫法
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
