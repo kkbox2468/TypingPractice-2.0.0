51fe8c9c77a54794c2039f1c4177d73cfe8c9967
Gao Ciaorun <itchynozz@gmail.com>
2020-05-20 19:40:32 +0800

    chart data color

--- app/javascript/scripts/stats.js
+++ app/javascript/scripts/stats.js
@@ -1,32 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var ctx = document.getElementById('chart')
-  if(!!ctx){
-    console.log(ctx)
-    ctx.getContext('2d');
-    var chart = new Chart(ctx, {
+  var accuracy = document.getElementById('chart')
+  if(!!accuracy){
+    console.log(accuracy)
+    accuracy.getContext('2d');
+    var chart = new Chart(accuracy, {
       type: 'doughnut',
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["accuracy", "total"],
         datasets: [{
             label: '# of Votes',
-            data: [32, 19, 30, 5, 2, 3],
+            data: [90, 10],
             backgroundColor: [
-                'rgba(255, 99, 132)',
-                'rgba(54, 162, 235)',
-                'rgba(255, 206, 86)',
-                'rgba(75, 192, 192)',
-                'rgba(153, 102, 255)',
-                'rgba(255, 159, 64)'
+                '#75ee70',
+                'rgb(70, 70, 70)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#75ee70',
+                'rgba(70, 70, 70,1)'
             ],
-            borderWidth: 4
+            borderWidth: 0
         }]
       }
     });
