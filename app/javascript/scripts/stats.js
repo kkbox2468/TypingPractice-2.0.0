a90ac0f3be7845adba56ed4e9b9215e9d8d28834
Gao Ciaorun <itchynozz@gmail.com>
2020-05-21 15:57:19 +0800

    wpm updated

--- app/javascript/scripts/stats.js
+++ app/javascript/scripts/stats.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let cur_accuracy = parseFloat(arti_accuracy.dataset['accuracy'])
   let accu_data = [cur_accuracy, 100 - cur_accuracy]
   if(!!arti_accuracy){ //確認有arti_accuracy有這個物件才執行（強制轉型）
-    console.log(arti_accuracy)
+    // console.log(arti_accuracy)
     arti_accuracy.getContext('2d');
     var chart = new Chart(arti_accuracy, {
       type: 'doughnut',
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var chart = new Chart(arti_wpm, {
       type: 'horizontalBar',
       data: {
-        labels: ["avg wpm"],
+        labels: ["Avg wpm"],
         datasets: [{
             label: 'wpm',
             data: wpm_data,
