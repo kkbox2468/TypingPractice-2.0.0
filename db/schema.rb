dcfe92c3fcdc8f963170ece8be1308799e189d40
Will <willie91734563@gmail.com>
2020-05-21 15:44:33 +0800

    add UserTopic model & ruby topic csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_023435) do
+ActiveRecord::Schema.define(version: 2020_05_21_055820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,19 @@ ActiveRecord::Schema.define(version: 2020_05_21_023435) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_user_articles_on_article_id"
     t.index ["user_id"], name: "index_user_articles_on_user_id"
+  end
+
+  create_table "user_topics", force: :cascade do |t|
+    t.integer "time"
+    t.integer "letter_count"
+    t.float "speed"
+    t.string "wrong_letter"
+    t.integer "wrong_letter_count"
+    t.float "accuracy"
+    t.integer "user_id"
+    t.integer "topic_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
