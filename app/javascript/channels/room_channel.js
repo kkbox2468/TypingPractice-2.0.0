0f360fea6f70567513a63e0b1fcb43bb00c37587
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-27 00:33:08 +0800

    fix playground-js

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,93 +1,100 @@
 import consumer from "./consumer"
 
-window.onload = function () {
-  let roomElement = document.querySelector('#room-id')
-  let roomId = roomElement.getAttribute('data-room-id')
-  let userElement = document.querySelector('#user-id')
-  let userId = Number(userElement.getAttribute('data-user-id'))
-  /* select for highlight characters */
-  let quoteInputRight = document.getElementById('racingQuoteInput2')
-  let quoteDisplayRight = document.getElementById('racingQuoteDisplay2')
-  /* select for topic scroll */
-  let quoteContainer = document.querySelector('.racing-ground')
-  let topicContainer = quoteContainer.clientHeight * 0.33 
-  let topicStartLine = document.querySelector('#racingQuoteTopic2')
-  /* select for ending page */
-  let pageBody = document.querySelector('body')
-  let endPage = document.createElement('div')
-  endPage.className = "end-page"
-  /* select for check */
-  let guestStatus = document.querySelector('.guest-status')
-  let guestCheck = document.querySelector('#guest-check')
+// window.onload = function () {
+$(function(){
+
+  if ( $('#rooms-show').length > 0 ){
+
+    let roomElement = document.querySelector('#room-id')
+    let roomId = roomElement.getAttribute('data-room-id')
+    let userElement = document.querySelector('#user-id')
+    let userId = Number(userElement.getAttribute('data-user-id'))
+    /* select for highlight characters */
+    let quoteInputRight = document.getElementById('racingQuoteInput2')
+    let quoteDisplayRight = document.getElementById('racingQuoteDisplay2')
+    /* select for topic scroll */
+    let quoteContainer = document.querySelector('.racing-ground')
+    let topicContainer = quoteContainer.clientHeight * 0.33 
+    let topicStartLine = document.querySelector('#racingQuoteTopic2')
+    /* select for ending page */
+    let pageBody = document.querySelector('body')
+    let endPage = document.createElement('div')
+    endPage.className = "end-page"
+    /* select for check */
+    let guestStatus = document.querySelector('.guest-status')
+    let guestCheck = document.querySelector('#guest-check')
 
 
-  consumer.subscriptions.create({ channel: "RoomChannel", room_id: roomId }, {
-    connected() {
-      // Called when the subscription is ready for use on the server
-      console.log('connected to ' + roomId );
-      
-    },
-  
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
-  
-    received(data) {
-      // Called when there's incoming data on the websocket for this channel
-      // console.log(data.content);
-      /* check players status */
-      if (data.check) {
-        if (userId !== data.user_id) {
-          if (data.check === "1") {
-            // guestStatus.innerText = "Ready"
-            guestStatus.classList.toggle('ready')
-            // console.log(guestCheck);
-            guestCheck.click();
+    consumer.subscriptions.create({ channel: "RoomChannel", room_id: roomId }, {
+      connected() {
+        // Called when the subscription is ready for use on the server
+        console.log('connected to ' + roomId );
+        
+      },
+    
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
+    
+      received(data) {
+        // Called when there's incoming data on the websocket for this channel
+        // console.log(data.content);
+        /* check players status */
+        if (data.check) {
+          if (userId !== data.user_id) {
+            if (data.check === "1") {
+              // guestStatus.innerText = "Ready"
+              guestStatus.classList.toggle('ready')
+              // console.log(guestCheck);
+              guestCheck.click();
+            } else {
+              // guestStatus.innerText = "Not Ready"
+              guestStatus.classList.toggle('ready')
+              guestCheck.click();
+            }
+          }
+        }
+
+
+        /* reder messages to other player's screen */
+        quoteInputRight.innerText = data.content
+        let arrayQuote = quoteDisplayRight.querySelectorAll('span');
+        let arrayValue = quoteInputRight.value.split('')
+        let inputIndex = quoteInputRight.value.length
+
+        if (data.message) {
+          if (userId !== data.message.user_id) {
+            checkCharacter(arrayQuote, arrayValue, inputIndex)
+            if (inputIndex === arrayQuote.length) {
+              Swal.fire({
+                title: '遊戲結束！',
+              }).then(() => {
+                window.location.replace('/playground/racing')
+              })
+            }
+          }
+        }
+
+
+        let selected = document.querySelector('.selected2')
+        let nextWord = selected.nextElementSibling
+        let gapAmount = (selected.offsetTop) - (topicStartLine.offsetTop)
+        let gapAmount2 = gapAmount - topicContainer
+    
+        if (gapAmount > topicContainer) {
+          if (selected.offsetTop < nextWord.offsetTop) {
+            $('#racingQuoteTopic2').css('margin-top',`-${gapAmount}px`)
           } else {
-            // guestStatus.innerText = "Not Ready"
-            guestStatus.classList.toggle('ready')
-            guestCheck.click();
+            $('#racingQuoteTopic2').css('margin-top',`-${gapAmount2}px`)
           }
-        }
+        } 
+
       }
+    });
+  }
+})
 
-
-      /* reder messages to other player's screen */
-      quoteInputRight.innerText = data.content
-      let arrayQuote = quoteDisplayRight.querySelectorAll('span');
-      let arrayValue = quoteInputRight.value.split('')
-      let inputIndex = quoteInputRight.value.length
-
-      if (data.message) {
-        if (userId !== data.message.user_id) {
-          checkCharacter(arrayQuote, arrayValue, inputIndex)
-          if (inputIndex === arrayQuote.length) {
-            Swal.fire({
-              title: '遊戲結束！',
-            }).then(() => {
-              window.location.replace('/playground/racing')
-            })
-          }
-        }
-      }
-
-
-      let selected = document.querySelector('.selected2')
-      let nextWord = selected.nextElementSibling
-      let gapAmount = (selected.offsetTop) - (topicStartLine.offsetTop)
-      let gapAmount2 = gapAmount - topicContainer
-  
-      if (gapAmount > topicContainer) {
-        if (selected.offsetTop < nextWord.offsetTop) {
-          $('#racingQuoteTopic2').css('margin-top',`-${gapAmount}px`)
-        } else {
-          $('#racingQuoteTopic2').css('margin-top',`-${gapAmount2}px`)
-        }
-      } 
-
-    }
-  });
-}
+// }
 
 function checkCharacter(arrayQuote, arrayValue, inputIndex) {
   arrayQuote.forEach((characterSpan, index) => {
