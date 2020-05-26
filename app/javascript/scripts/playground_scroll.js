b0584719f45c12bd5e37ce17f2074a8c13bf123e
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-26 21:15:11 +0800

    fix playground-js

--- app/javascript/scripts/playground_scroll.js
+++ app/javascript/scripts/playground_scroll.js
@@ -4,18 +4,13 @@ $(function(){
     $('#quoteInput').on('keydown', function(){
       let container = document.querySelector('.play-container') 
       let topicContain = container.clientHeight * 0.33
-      // console.log(topicContain)
   
       let startLine = document.querySelector('#quotetopic')
       let selected = document.querySelector('.selected')
-      let nextWord = selected.nextElementSibling
-      // console.log(startLine.offsetTop)
-      // console.log(selected.offsetTop)
-      // console.log(nextWord.offsetTop)
+      let nextWord = selected.nextElementSibling.innerHTML
+      console.log(nextWord)
       let gapAmount = (selected.offsetTop) - (startLine.offsetTop)
-      // console.log(gapAmount)
       let gapAmount2 = gapAmount - topicContain
-      // console.log(gapAmount2)
   
       if (gapAmount > topicContain) {
         if (selected.offsetTop < nextWord.offsetTop) {
