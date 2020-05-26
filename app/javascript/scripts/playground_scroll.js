212dcde66818d18220ae93fabb0ea04ee1f5dd3b
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-26 18:37:22 +0800

    sort all .js

--- app/javascript/scripts/playground_scroll.js
+++ app/javascript/scripts/playground_scroll.js
@@ -1,28 +1,29 @@
 $(function(){
+  if ( $('#coding-show').length > 0 || $('#typing-show').length > 0 ){
 
-  $('#quoteInput').on('keydown', function(){
-    let container = document.querySelector('.play-container') 
-    let topicContain = container.clientHeight * 0.33
-    // console.log(topicContain)
-
-    let startLine = document.querySelector('#quotetopic')
-    let selected = document.querySelector('.selected')
-    let nextWord = selected.nextElementSibling
-    // console.log(startLine.offsetTop)
-    // console.log(selected.offsetTop)
-    // console.log(nextWord.offsetTop)
-    let gapAmount = (selected.offsetTop) - (startLine.offsetTop)
-    // console.log(gapAmount)
-    let gapAmount2 = gapAmount - topicContain
-    // console.log(gapAmount2)
-
-    if (gapAmount > topicContain) {
-      if (selected.offsetTop < nextWord.offsetTop) {
-        $('#quotetopic').css('margin-top',`-${gapAmount}px`)
-      } else {
-        $('#quotetopic').css('margin-top',`-${gapAmount2}px`)
-      }
-    } 
-  })
+    $('#quoteInput').on('keydown', function(){
+      let container = document.querySelector('.play-container') 
+      let topicContain = container.clientHeight * 0.33
+      // console.log(topicContain)
   
+      let startLine = document.querySelector('#quotetopic')
+      let selected = document.querySelector('.selected')
+      let nextWord = selected.nextElementSibling
+      // console.log(startLine.offsetTop)
+      // console.log(selected.offsetTop)
+      // console.log(nextWord.offsetTop)
+      let gapAmount = (selected.offsetTop) - (startLine.offsetTop)
+      // console.log(gapAmount)
+      let gapAmount2 = gapAmount - topicContain
+      // console.log(gapAmount2)
+  
+      if (gapAmount > topicContain) {
+        if (selected.offsetTop < nextWord.offsetTop) {
+          $('#quotetopic').css('margin-top',`-${gapAmount}px`)
+        } else {
+          $('#quotetopic').css('margin-top',`-${gapAmount2}px`)
+        }
+      } 
+    })
+  }
 })
--- app/javascript/scripts/stats.js
+++ app/javascript/scripts/stats.js
@@ -1,56 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let arti_accuracy = document.getElementById('arti_accuracy')
-  let cur_accuracy = parseFloat(arti_accuracy.dataset['accuracy'])
-  let accu_data = [cur_accuracy, 100 - cur_accuracy]
-  if(!!arti_accuracy){ //確認有arti_accuracy有這個物件才執行（強制轉型）
-    // console.log(arti_accuracy)
-    arti_accuracy.getContext('2d');
-    var chart = new Chart(arti_accuracy, {
-      type: 'doughnut',
-      data: {
-        labels: ["Accuracy", "Inaccuracy"],
-        datasets: [{
-            label: 'Accuracy',
-            data: accu_data,
-            backgroundColor: [
-                '#3cce68',
-                '#ff7091'
-            ],
-            borderColor: [
-                '#3cce68',
-                '#ff7091'
-            ],
-            borderWidth: 0
-        }]
+$(function(){
+  if ( $('#stats-index').length > 0 ){
+    document.addEventListener("DOMContentLoaded", function() {
+      let arti_accuracy = document.getElementById('arti_accuracy')
+      let cur_accuracy = parseFloat(arti_accuracy.dataset['accuracy'])
+      let accu_data = [cur_accuracy, 100 - cur_accuracy]
+      if(!!arti_accuracy){ //確認有arti_accuracy有這個物件才執行（強制轉型）
+        // console.log(arti_accuracy)
+        arti_accuracy.getContext('2d');
+        var chart = new Chart(arti_accuracy, {
+          type: 'doughnut',
+          data: {
+            labels: ["Accuracy", "Inaccuracy"],
+            datasets: [{
+                label: 'Accuracy',
+                data: accu_data,
+                backgroundColor: [
+                    '#3cce68',
+                    '#ff7091'
+                ],
+                borderColor: [
+                    '#3cce68',
+                    '#ff7091'
+                ],
+                borderWidth: 0
+            }]
+          }
+        });
       }
     });
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  let arti_wpm = document.getElementById('arti_wpm')
-  let cur_wpm =  parseFloat(arti_wpm.dataset['wpm'])
-  let wpm_data = [cur_wpm ]
-  if(!!arti_wpm){ 
-    // console.log(arti_wpm)
-    arti_wpm.getContext('2d');
-    var chart = new Chart(arti_wpm, {
-      type: 'horizontalBar',
-      data: {
-        // labels: ["Avg wpm"],
-        datasets: [{
-            label: 'wpm',
-            data: wpm_data,
-            backgroundColor: [
-                '#6ac6ce'
-            ],
-            borderColor: [
-                '#6ac6ce'
-            ],
-            borderWidth: 0
-        }]
+    
+    document.addEventListener("DOMContentLoaded", function() {
+      let arti_wpm = document.getElementById('arti_wpm')
+      let cur_wpm =  parseFloat(arti_wpm.dataset['wpm'])
+      let wpm_data = [cur_wpm ]
+      if(!!arti_wpm){ 
+        // console.log(arti_wpm)
+        arti_wpm.getContext('2d');
+        var chart = new Chart(arti_wpm, {
+          type: 'horizontalBar',
+          data: {
+            // labels: ["Avg wpm"],
+            datasets: [{
+                label: 'wpm',
+                data: wpm_data,
+                backgroundColor: [
+                    '#6ac6ce'
+                ],
+                borderColor: [
+                    '#6ac6ce'
+                ],
+                borderWidth: 0
+            }]
+          }
+        });
       }
-    });
+    })
   }
 })
+
 
