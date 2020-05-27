dee9421d7023df64c394847cbadf60ca94bc9d64
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-05-28 00:45:38 +0800

    fix if no user_topics can still play

--- app/controllers/playground_controller.rb
+++ app/controllers/playground_controller.rb
@@ -26,7 +26,6 @@ class PlaygroundController < ApplicationController
       articles_count = articles.count
       codes_count = codes.count
 
-
       unless current_user.user_topics.empty?
         @article_progress = (article_progress.each.map{|i|i.accuracy}.reduce(:+)/articles_count).floor(2)
         @code_progress = (code_progress.each.map{|i|i.accuracy}.reduce(:+)/codes_count).floor(2)
