87ad131b659f5db3ffb6ba7316c2a17606425047
Joseph Hung <arixxle@gmail.com>
2020-05-28 18:26:12 +0800

    add room members

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -2,15 +2,21 @@ class RoomChannel < ApplicationCable::Channel
   @@count = 0
   def subscribed
     if params[:room_id]
+      room = Room.find_by(id: params[:room_id])
       stream_from "room_channel_#{params[:room_id]}"
-      @@count += 1
+      room.members += 1
+      room.save
+      # @@count += 1
     end
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
     stop_all_streams
-    @@count -= 1
+    room = Room.find_by(id: params[:room_id])
+    room.members -= 1
+    room.save
+    # @@count -= 1
   end
 
   def self.counter
