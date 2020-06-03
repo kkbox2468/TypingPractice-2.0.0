60703b084bf647412819a987c34ec85150d09ab1
Will <willie91734563@gmail.com>
2020-06-03 11:36:06 +0800

    刪註解

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,7 +22,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-  config.mailer = 'ContactMailer'
+  # config.mailer = 'ContactMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
