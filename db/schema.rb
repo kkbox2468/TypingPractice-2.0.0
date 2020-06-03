8b6ee45687ac7384b32013b6964e2d5894c89f9d
Joseph Hung <arixxle@gmail.com>
2020-06-03 18:19:34 +0800

    fix bugs

--- db/schema.rb
+++ db/schema.rb
@@ -123,10 +123,6 @@ ActiveRecord::Schema.define(version: 2020_05_30_104321) do
     t.string "nickname"
     t.string "provider"
     t.string "uid"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
