0aefc7f424bb95f1c5c38093eb38494e13fb334d
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-06-04 20:23:47 +0800

    add star for progress

--- app/controllers/typing_controller.rb
+++ app/controllers/typing_controller.rb
@@ -10,9 +10,20 @@ class TypingController < ApplicationController
 
       topics.each do |topic|
         progress = current_user.user_topics.where(topic_id: topic.topic_id).order('accuracy').last
-        if progress
-          @all_progress.merge!({topic.topic_id => progress.accuracy})
-        end 
+
+          case progress.accuracy
+          when 100
+            @all_progress.merge!({topic.topic_id => 5})
+          when (80.0..99.0)
+            @all_progress.merge!({topic.topic_id => 4})
+          when (60.0..79.0) 
+            @all_progress.merge!({topic.topic_id => 3})
+          when (40.0..59.0)
+            @all_progress.merge!({topic.topic_id => 2})
+          else (0.1..39.0)
+            @all_progress.merge!({topic.topic_id => 1})
+          end
+          
       end
       # u1.user_topics.where(topic_id: 1).order('accuracy').last
       # u1.user_topics.select('Max(accuracy), topic_id').group(:topic_id).all
--- app/javascript/scripts/playground/playground_progress.js
+++ app/javascript/scripts/playground/playground_progress.js
@@ -7,23 +7,48 @@ $(function(){
       accracyArray.push($('.accracy')[eachAccracy].innerHTML)
       eachAccracy += 1
     }
+    console.log(accracyArray)
 
-    eachProgress = 0
-    progressArray = []
-    for (let i = 0; i < $('.progress-bar').length; i++) {
-      progressArray.push($('.progress-bar')[eachProgress])
-      eachProgress += 1
-    }
+    // eachProgress = 0
+    // progressArray = []
+    // for (let i = 0; i < $('.progress-bar').length; i++) {
+    //   progressArray.push($('.progress-bar')[eachProgress])
+    //   eachProgress += 1
+    // }
+    // console.log(progressArray)
 
+    let n = 1
     for (let i = 0; i < accracyArray.length; i++) {
-      $(progressArray[i]).css("width", `${accracyArray[i]}%`)
+
+      console.log($(`#lessonBar${n}`))
+      console.log(Number(accracyArray[i]))
+      let fullStars = Number(accracyArray[i])
+      function giveFullStar() {
+        $(`#lessonBar${n}`).append(`<img src="/icon/star-full.png" alt="..." >`)  
+      }
+      let stars = 5 - Number(accracyArray[i])
+      function giveStar() {
+        $(`#lessonBar${n}`).append(`<img src="/icon/star-emtpy.png" alt="..." >`)  
+      }
+      
+      if (fullStars > 0){
+        for (let n = 0; n < fullStars; n++) {
+          giveFullStar();
+        }
+      }
+      if (stars > 0){
+        for (let n = 0; n < stars; n++) {
+          giveStar();
+        }
+      }
+      n += 1
     }
   }
   
   if ( $('#playground-index').length > 0 ){
     // playground判斷
     $('#typingModeBar .progress-bar').css("width", `${$(".article_progress")[0].innerText}%`)
-    $('#codeModeBar .progress-bar').css("width", `${$(".code_progress")[0].innerText}%`)
+    $('#lessonBar .progress-bar').css("width", `${$(".code_progress")[0].innerText}%`)
 
   }
 })
