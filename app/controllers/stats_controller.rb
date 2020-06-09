810f54fb74ca72c1143bf763ca6c3ada6279b2e4
Will <willie91734563@gmail.com>
2020-06-09 18:50:50 +0800

    更改判斷權限寫法

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,26 +1,21 @@
 class StatsController < ApplicationController
-  
+  include Accessible
 
   def index
     
-    if current_user
+    
       find_type_history   
       
       @arti_accuracy = current_user.user_topics.average(:accuracy).to_i #從current user抓底下create的所有文章裡的準確度.average可以算平均再算成整數
-<<<<<<< HEAD
       @arti_wpm = current_user.user_topics.where.not(speed:nil).where("speed != ?", "Infinity").average(:speed).to_i
       # render json: @arti_wpm
-=======
-      @arti_wpm = current_user.user_topics.average(:speed).to_i
-      
->>>>>>> master
       #('因為兩張資料表有一樣的created_at,所以要再給一次所屬資料表')
       @arti_day = current_user.user_topics.where("updated_at > ?", Date.today - 6.days).group_by_day('user_topics.updated_at', format: "%m""%d").average(:accuracy) #show日hash
       # render json: @day_week = @arti_day#.values.map(&:to_i) #抓出weekday
 
-      #######此代碼無使用######
+      
       @arti_day_accu = current_user.user_topics.where("updated_at > ?", Date.today - 6.days).group_by_day('user_topics.updated_at').average(:accuracy)
-      #######################
+      
 
 
       # render json: @arti_day_accu.values.map(&:to_i)#下排簡寫
@@ -40,42 +35,18 @@ class StatsController < ApplicationController
       # @topic_most_wrong = @topic_wrong_letter
       # render json: @topic_wrong_letter
       
-    else
-      ####應轉到註冊頁########
-      # redirect_to new_user_session_path 或使用include Accessible
-      redirect_to typing_index_path, notice: 'Not a member?Join us to record achievements!'
-      ######################
-    end
-
-    
-    
     
   end
   
-  
-  
-
-    
   
   private
   
   def find_type_history 
     user_topics = current_user.user_topics.where.not(accuracy: nil)
-<<<<<<< HEAD
-
-    ####只有首字變大寫，其他字沒變小寫########
-    user_topics = user_topics.joins(:topic).where('topics.type = ?', params[:type].camelize) if params[:type].present?
-    #####################################
-
-    ############# 排序特意由大到小的原因是？？##############
-    @user_topics = user_topics.order(id: :desc).limit(10)
-    #############################################
-=======
     # user_topics = current_user.user_topics.where.not(speed: "NaN")
     # user_topics = user_topics.joins(:topic).where('topics.type = ?', params[:type].camelize) if params[:type].present?
     @user_topics = user_topics.order(id: :desc).limit(10)
     # user_topics = current_user.user_topics.where.not(wrong_letter: nil)
->>>>>>> master
   
   end
 
