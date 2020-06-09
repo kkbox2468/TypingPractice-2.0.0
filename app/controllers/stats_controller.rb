67a6735b0b785f83a30979eca5e8d7e1f470f009
Will <willie91734563@gmail.com>
2020-06-09 11:57:29 +0800

    WIP states

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,6 @@
 class StatsController < ApplicationController
+  
+
   def index
     # @user_topics = current_user.user_topics(:accuracy).all
     # render json: @user_topics
@@ -6,20 +8,32 @@ class StatsController < ApplicationController
       find_type_history   
       
       @arti_accuracy = current_user.user_topics.average(:accuracy).to_i #從current user抓底下create的所有文章裡的準確度.average可以算平均再算成整數
-      @arti_wpm = current_user.user_topics.average(:speed).to_i
+      @arti_wpm = current_user.user_topics.where.not(speed:nil).where("speed != ?", "Infinity").average(:speed).to_i
       # render json: @arti_wpm
       #('因為兩張資料表有一樣的created_at,所以要再給一次所屬資料表')
       @arti_day = current_user.user_topics.where("updated_at > ?", Date.today - 6.days).group_by_day('user_topics.updated_at', format: "%m""%d").average(:accuracy) #show日hash
       # render json: @day_week = @arti_day#.values.map(&:to_i) #抓出weekday
+
+      #######此代碼無使用######
       @arti_day_accu = current_user.user_topics.where("updated_at > ?", Date.today - 6.days).group_by_day('user_topics.updated_at').average(:accuracy)
+      #######################
+
+
       # render json: @arti_day_accu.values.map(&:to_i)#下排簡寫
       # render json: @acc_day = @arti_day_accu.values.map{ |e| e.to_i} #抓出正確率整數
 
       # @article_accuracy_presenter = ArticleAccuracyPresenter.new(current_user).perform
+
+      #######此代碼無使用######
       @today_topic = current_user.user_topics.where("updated_at > ?",  Date.today).group_by_day('user_topics.updated_at').count
+      #######################
+
       # render json: @today_topic
     else
+      ####應轉到註冊頁########
+      # redirect_to new_user_session_path 或使用include Accessible
       redirect_to typing_index_path, notice: 'Not a member?Join us to record achievements!'
+      ######################
     end
   
   end
@@ -28,8 +42,14 @@ class StatsController < ApplicationController
   
   def find_type_history 
     user_topics = current_user.user_topics.where.not(accuracy: nil)
+
+    ####只有首字變大寫，其他字沒變小寫########
     user_topics = user_topics.joins(:topic).where('topics.type = ?', params[:type].camelize) if params[:type].present?
+    #####################################
+
+    ############# 排序特意由大到小的原因是？？##############
     @user_topics = user_topics.order(id: :desc).limit(10)
+    #############################################
   
   end
 end
