6f88eedf93d6f068a80a83fdd56bcb928b129244
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-06-09 11:58:43 +0800

    adjust navbar css

--- app/javascript/channels/invitation_channel.js
+++ app/javascript/channels/invitation_channel.js
@@ -15,7 +15,7 @@ consumer.subscriptions.create("InvitationChannel", {
     $('#invitation-sand').append(`
       <a href="/playground/racing/rooms/` + data.room_id + `">
         <img src="/icon/swords.png" alt="">
-        <p>Invitation Form ` + data.user_id + `</p>
+        <p>Invitation From ` + data.owner_name + `</p>
       </a>
     `);
     $('.icon-for-new-post').css("display", "flex");
@@ -30,9 +30,10 @@ $(function(){
       let thisRoomId = this.id
       let thisRoomName = $(`#${thisRoomId}`).data('room-name')
       let thisRoomNum = $(`#${thisRoomId}`).data('room-id')
+      let thisOwner = $(`#${thisRoomId}`).data('owner-name')
       
       $('#invitation_content')[0].value = thisRoomName
-      $('#invitation_room_id')[0].value = thisRoomNum
+      $('#invitation_owner_name')[0].value = thisOwner
       $('input#sand_news')[0].click();
     })
   }
--- app/reflexes/profile_reflex.rb
+++ app/reflexes/profile_reflex.rb
@@ -5,7 +5,6 @@ class ProfileReflex < ApplicationReflex
     @user = User.find(element.dataset[:id])
     @arti_accuracy = @user.user_topics.average(:accuracy).to_i 
 
-    # byebug
     @user.increment! :heart_counter
     cable_ready["heart_count"].text_content(
       selector: "#user-#{@user.id}-likes",
