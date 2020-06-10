5532622909eaf42b7852cb98605900694d246fd4
Will <willie91734563@gmail.com>
2020-06-10 17:52:37 +0800

    已有重複信箱或暱稱存在顯示notice 及 修復註冊失敗session為nil之錯誤

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,8 +52,10 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+    
+      if data = session["devise.facebook_data"] #&& session["devise.facebook_data"]["extra"]["raw_info"]
+        # user.email = data["email"] if user.email.blank?
+        user.email = session["devise.facebook_data"]["info"]["email"] if user.email.blank?
       end
     end
   end
