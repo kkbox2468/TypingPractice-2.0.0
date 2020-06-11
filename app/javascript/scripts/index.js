67f27e12ab8b37804bd4f4acb6f876097139230c
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-06-11 16:27:29 +0800

    fix setting form

--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -3,11 +3,7 @@ import './stats.js'
 import './playground/playground.js'
 import './playground/playground_alert.js'
 import './playground/playground_scroll.js'
-
 import './playground/playground_setting.js'
-
 import './playground/playground_racing.js'
-
-
 import './playground/playground_progress.js'
 
